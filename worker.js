5406a98c5c40fa681aeb647475735a1ce82b7fb4
koszulinski-cell <koszulinski@gmail.com>
2026-08-23 14:02:02 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,3 +1,49 @@
-import api from "./data.js";
+import { getPoliceStatus } from "./police.js";
+import { getIceStatus } from "./ice.js";
 
-export default api;
+export default {
+
+  async fetch(request, env) {
+
+    const url = new URL(request.url);
+
+    /*
+     * API
+     */
+
+    if (url.pathname === "/api/status") {
+
+      const [police, ice] =
+        await Promise.all([
+          getPoliceStatus(),
+          getIceStatus()
+        ]);
+
+      const result = {
+
+        updated: new Date().toISOString(),
+
+        police,
+
+        ice
+      };
+
+      return new Response(
+        JSON.stringify(result, null, 2),
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "Cache-Control": "no-store"
+          }
+        }
+      );
+    }
+
+    /*
+     * Everything else:
+     * serve the website from Cloudflare.
+     */
+
+    return env.ASSETS.fetch(request);
+  }
+};
